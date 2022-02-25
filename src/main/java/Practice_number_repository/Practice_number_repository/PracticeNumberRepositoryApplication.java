35567d1defacd9332df661c49daf13e3b422f5b7
YOOBINNOH <kuninyb@naver.com>
2022-02-25 16:41:58 +0900

    < 개인 프로젝트 실습 >
    숫자로 회원가입 하기. 중복 숫자는 불가.
    
    - H2 데이터 베이스 실습해보기
    - Test 코드 작성 후 검증해보기
    - 어노테이션 사용해보기

---
+++ src/main/java/Practice_number_repository/Practice_number_repository/PracticeNumberRepositoryApplication.java
@@ -0,0 +1,13 @@
+package Practice_number_repository.Practice_number_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeNumberRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeNumberRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Practice_number_repository/Practice_number_repository/PracticeNumberRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package Practice_number_repository.Practice_number_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeNumberRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
